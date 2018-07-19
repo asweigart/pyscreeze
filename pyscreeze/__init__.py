ec4d181551ebfa081ec635afeef39a59851b0d09
Al Sweigart <asweigart@gmail.com>
2018-07-19 12:24:42 -0700

    Fixing MANIFEST.in to include readme, which cause installation failure.

--- pyscreeze/__init__.py
+++ pyscreeze/__init__.py
@@ -10,7 +10,7 @@ https://stackoverflow.com/questions/7648200/pip-install-pil-e-tickets-1-no-jpeg-
 http://ubuntuforums.org/showthread.php?t=1751455
 """
 
-__version__ = '0.1.17'
+__version__ = '0.1.18'
 
 import datetime
 import os
