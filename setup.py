9119cee9ffaf4676ccbacf567e9203c3c6f29a8a
Al Sweigart <asweigart@gmail.com>
2019-10-09 11:35:49 -0700

    Ran Black over setup.py

--- setup.py
+++ setup.py
@@ -7,43 +7,44 @@ scriptFolder = os.path.dirname(os.path.realpath(__file__))
 os.chdir(scriptFolder)
 
 # Load version from module (without loading the whole module)
-with open('pyscreeze/__init__.py', 'r') as fileObj:
-    version = re.search(r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                        fileObj.read(), re.MULTILINE).group(1)
+with open("pyscreeze/__init__.py", "r") as fileObj:
+    version = re.search(
+        r'^__version__\s*=\s*[\'"]([^\'"]*)[\'"]', fileObj.read(), re.MULTILINE
+    ).group(1)
 
 # Use the README.md content for the long description:
-with io.open('README.md', encoding='utf-8') as fileObj:
+with io.open("README.md", encoding="utf-8") as fileObj:
     long_description = fileObj.read()
 
 setup(
-    name='PyScreeze',
+    name="PyScreeze",
     version=version,
-    url='https://github.com/asweigart/pyscreeze',
-    author='Al Sweigart',
-    author_email='al@inventwithpython.com',
-    description='A simple, cross-platform screenshot module for Python 2 and 3.',
+    url="https://github.com/asweigart/pyscreeze",
+    author="Al Sweigart",
+    author_email="al@inventwithpython.com",
+    description="A simple, cross-platform screenshot module for Python 2 and 3.",
     long_description=long_description,
-    license='MIT',
-    packages=['pyscreeze'],
-    test_suite='tests',
-    install_requires=['Pillow'],
-    requires_python='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*', # Copied from the Pillow library (October 2019), since PyScreeze is built on top of it.
+    license="MIT",
+    packages=["pyscreeze"],
+    test_suite="tests",
+    install_requires=["Pillow"],
+    requires_python=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",  # Copied from the Pillow library (October 2019), since PyScreeze is built on top of it.
     keywords="screenshot screen screencap capture scrot screencapture image",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Win32 (MS Windows)',
-        'Environment :: X11 Applications',
-        'Environment :: MacOS X',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-         # Copied from the Pillow library (October 2019), since PyScreeze is built on top of it:
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 4 - Beta",
+        "Environment :: Win32 (MS Windows)",
+        "Environment :: X11 Applications",
+        "Environment :: MacOS X",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        # Copied from the Pillow library (October 2019), since PyScreeze is built on top of it:
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
 )
